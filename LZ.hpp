c0d1fc2b0d055fe02acd773f1b733e1e75cd44b7
GKPedrsen <GKPedrsen@gmail.com>
2024-08-14 10:15:15 +0200

    add LZ77::Compress(..)

---
+++ LZ.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <string_view>
+#include <tuple>
+#include <vector>
+#include <cassert>
+
+namespace _detail
+{
+
+std::string_view findLongestMatch(const char* searchBegin, const char* lookaheadBegin, const char* lookaheadEnd)
+{
+  assert(searchBegin <= lookaheadBegin);
+  assert(lookaheadBegin <= lookaheadEnd);
+
+  size_t currLen = 0;
+  size_t longestLen = 0;
+  const char* longestMatch = nullptr;
+
+  auto updateLongest = [&](const char* strEnd)
+  {
+    if(currLen == 0)
+      return;
+
+    if(currLen >= longestLen)
+    {
+      longestLen = currLen;
+      longestMatch = strEnd - currLen;
+    }
+
+    currLen = 0;
+  };
+
+  const char* sCurs = searchBegin;
+  const char* lCurs = lookaheadBegin;
+
+  while(lCurs <= lookaheadEnd && searchBegin < lookaheadBegin)
+  {
+    if(*sCurs != *lCurs)
+    {
+      //cut first byte of search buffer
+      ++searchBegin;
+
+      //reset cursors
+      lCurs = lookaheadBegin;
+      sCurs = searchBegin;
+
+      //update longest & skip
+      updateLongest(sCurs);
+      continue;
+    }
+
+    ++currLen;
+    ++lCurs;
+    ++sCurs;
+  }
+
+  updateLongest(sCurs);
+
+  return {longestMatch, longestLen};
+}
+
+} //namespace: _detail
+
+namespace LZ77
+{
+
+using Token = std::tuple<unsigned int, unsigned int, char>;
+
+std::vector<Token> 
+Compress(std::string_view input, size_t searchSize=32768, size_t lookaheadSize=1024)
+{
+  std::vector<Token> compressed;
+
+  //slide window across input
+  for(size_t i = 0; i < input.size(); ++i)
+  {
+    //Pointers that define the search & lookahead buffers
+    //
+    //"foobarbazfoobarbazfoobarbazfoobarbazfoobarbazfoobarbaz"
+    //      |              |                   |
+    //  searchBegin   lookaheadBegin      lookaheadEnd
+    const char* searchBegin    = (input.data() + i) - std::min(searchSize, i);
+    const char* lookaheadBegin = (input.data() + i);
+    const char* lookaheadEnd   = (input.data() + i) + std::min(lookaheadSize, (input.size()-1) - i);
+
+    auto match = 
+      _detail::findLongestMatch(searchBegin, lookaheadBegin, lookaheadEnd);
+
+    if(match.data())
+    {
+      compressed.emplace_back(lookaheadBegin - match.data(), match.length(), '-');
+      i += match.length() - 1;
+      continue;
+    }
+
+    compressed.emplace_back(0, 0, *lookaheadBegin);
+  }
+
+  return compressed;
+}
+
+} //namespace: LZ77
+namespace LZ1 = LZ77;
+
